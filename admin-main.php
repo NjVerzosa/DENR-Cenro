e47c6fb9b020d4014fa67cd13209bd001a38d8a5
Nj Verzosa <njverzosa24@gmail.com>
2024-02-19 16:15:27 -0800

    .

--- admin-main.php
+++ admin-main.php
@@ -29,7 +29,7 @@ if (isset($_POST['userInput'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Officer Main</title>
+    <title>Admin Main</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
